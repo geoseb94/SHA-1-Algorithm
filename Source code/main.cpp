f191a4e78e808cad9ef835c81ece1c69966d2ceb
geoseb94 <57502986+geoseb94@users.noreply.github.com>
2020-05-01 00:43:12 +0200

    Add files via upload

---
+++ Source code/main.cpp
@@ -0,0 +1,19 @@
+/***************************************************************************
+*============= Copyright by Darmstadt University of Applied Sciences =======
+****************************************************************************
+* Filename        : sha-1.cpp
+* Author          : Jishnu Murali Thampan(762574), George Sebastian(762610)
+* Description     : Driver main for SHA-1 Alogrithm
+* 					Note: This version does not support message sizes > 512 bytes
+****************************************************************************/
+#include "sha-1.h"
+
+int main(void)
+{
+	 generate_sha1_hash();
+	 return 0;
+}
+
+
+
+
